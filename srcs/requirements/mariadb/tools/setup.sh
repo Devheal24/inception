4cfcd456cc887137e1926e7f9cce4b31070fdeb0
Mickaël GARNIER <mickaelgar@gmail.com>
2026-07-01 12:15:00 +0200

    mariadb configured

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+    mysqld --user=mysql --bootstrap << EOF
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* 
+TO '${MYSQL_USER}'@'%';
+
+FLUSH PRIVILEGES;
+
+EOF
+
+fi
+
+exec mysqld --user=mysql --console
