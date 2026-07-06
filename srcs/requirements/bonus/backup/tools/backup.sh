acbc596f4adaf463d488997d1929b14e7b82194b
mgarnier <mgarnier@2E1.42angouleme.fr>
2026-07-06 15:05:45 +0200

    add protection backup.sh

--- srcs/requirements/bonus/backup/tools/backup.sh
+++ srcs/requirements/bonus/backup/tools/backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 BACKUP_DIR=/backups
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
@@ -9,10 +10,13 @@ echo "$(date '+%F %T') [backup] starting backup"
 
 mariadb-dump -h mariadb -uwpuser -p"${MYSQL_PASSWORD}" wordpress \
     | gzip > "${BACKUP_DIR}/mariadb_${TIMESTAMP}.sql.gz"
+
 echo "$(date '+%F %T') [backup] mariadb dump done"
 
 tar -czf "${BACKUP_DIR}/wordpress_${TIMESTAMP}.tar.gz" -C /var/www/html .
+
 echo "$(date '+%F %T') [backup] wordpress archive done"
 
 find "${BACKUP_DIR}" -name '*.gz' -mtime "+${RETENTION_DAYS}" -delete
+
 echo "$(date '+%F %T') [backup] backup complete"
